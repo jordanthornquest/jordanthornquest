4da19baae6a900bd7133f602fe63cdba4cbf21c2
Jordan Thornquest <me@jordanthornquest.com>
2019-03-08 02:46:18 +0000

    move service-worker.js to root

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -20,7 +20,7 @@ WebFontConfig = {
 // ServiceWorker is a progressive technology. Ignore unsupported browsers
 if ('serviceWorker' in navigator) {
   console.log('CLIENT: service worker registration in progress.');
-  navigator.serviceWorker.register('/assets/js/service-worker.js').then(function() {
+  navigator.serviceWorker.register('/service-worker.js').then(function() {
     console.log('CLIENT: service worker registration complete.');
   }, function() {
     console.log('CLIENT: service worker registration failure.');
