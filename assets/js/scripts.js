db97b19203b1cc64b1bd98e4ef47d57d0ca8c73e
Jordan Thornquest <me@jordanthornquest.com>
2019-03-08 02:21:07 +0000

    add service worker

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -16,3 +16,15 @@ WebFontConfig = {
   wf.async = true;
   s.parentNode.insertBefore(wf, s);
 })(document);
+
+// ServiceWorker is a progressive technology. Ignore unsupported browsers
+if ('serviceWorker' in navigator) {
+  console.log('CLIENT: service worker registration in progress.');
+  navigator.serviceWorker.register('service-worker.js').then(function() {
+    console.log('CLIENT: service worker registration complete.');
+  }, function() {
+    console.log('CLIENT: service worker registration failure.');
+  });
+} else {
+  console.log('CLIENT: service worker is not supported.');
+}
