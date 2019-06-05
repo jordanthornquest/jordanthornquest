79569c00c16dc0156aa06b8d1d2a0778b4de9751
Jordan Thornquest <me@jordanthornquest.com>
2019-06-04 22:21:30 -0600

    Use jekyll assets

--- service-worker.js
+++ service-worker.js
@@ -21,7 +21,6 @@ const RUNTIME = 'runtime';
 const PRECACHE_URLS = [
   'index.html',
   '/', // Alias for index.html
-  '/assets/css/style.css'
 ];
 
 // The install handler takes care of precaching the resources we always need.
