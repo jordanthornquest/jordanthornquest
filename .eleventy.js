ac4f538b78937f209d79626a8e7844f81988c937
Jordan Thornquest <1444819+jordanthornquest@users.noreply.github.com>
2021-03-05 05:58:50 +0000

    Add Eleventy vue

--- .eleventy.js
+++ .eleventy.js
@@ -1,9 +1,17 @@
+// Libraries
 const eleventyVue = require("@11ty/eleventy-plugin-vue");
 
+// Export configuration
 module.exports = (eleventyConfig) => {
+  // Add eleventyVue plugin
   eleventyConfig.addPlugin(eleventyVue);
+
+  // Add passthrough copy for image files
   eleventyConfig.addPassthroughCopy("src/images");
+
+  // Add passthrough copy for web manifest
   eleventyConfig.addPassthroughCopy("src/manifest.webmanifest");
+
   // Return config object
   return {
     dir: {
