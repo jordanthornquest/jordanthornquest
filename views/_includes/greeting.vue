da9023f77e795572b5a325e6e382ad86d4f31878
Jordan Thornquest <1444819+jordanthornquest@users.noreply.github.com>
2021-03-06 07:59:12 +0000

    add greeting component

---
+++ views/_includes/greeting.vue
@@ -0,0 +1,16 @@
+<template>
+  <header>
+    <h1>Reconstructing, be back soon.</h1>
+    <p>
+      For the time being, check out my
+      <a href="https://music.jordanthornquest.com">Bandcamp</a>. See you
+      shortly!
+    </p>
+  </header>
+</template>
+
+<style>
+h1 {
+  color: red;
+}
+</style>
