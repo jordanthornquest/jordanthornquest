0cabbc85a0866398d885aa63de6e03ad8a116f73
Jordan Thornquest <1444819+jordanthornquest@users.noreply.github.com>
2021-03-06 07:02:02 +0000

    Add data for index

---
+++ views/index.11tydata.js
@@ -0,0 +1,15 @@
+// Libraries
+const path = require("path");
+
+// Export data
+module.exports = {
+  description:
+    "Idaho musician Jordan Thornquest maps his mindstate with theatrical relish, producing a form of punk-informed power pop that's both candidly introspective and fearlessly extroverted.",
+  layout: "base.html",
+  socialImage:
+    "https://res.cloudinary.com/deedobzcr/image/upload/q_auto,f_auto,w_1200/v1559763975/hurr.jpg",
+  title: "Home",
+  eleventyComputed: {
+    url: (data) => path.join(data.constants.siteUrl, data.page.url),
+  },
+};
