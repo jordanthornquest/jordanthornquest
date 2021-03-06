b3cff58f955737cdba468d5a28fe710e33639dfb
Jordan Thornquest <1444819+jordanthornquest@users.noreply.github.com>
2021-03-06 07:59:06 +0000

    Add postcss config

--- .eleventy.js
+++ .eleventy.js
@@ -3,7 +3,14 @@ const eleventyVue = require("@11ty/eleventy-plugin-vue");
 
 module.exports = function (eleventyConfig) {
   // Add Eleventy Vue plugin, use defaults
-  eleventyConfig.addPlugin(eleventyVue);
+  eleventyConfig.addPlugin(eleventyVue, {
+    // Use postcss in the single file components
+    rollupPluginVueOptions: {
+      style: {
+        postcssPlugins: [require("autoprefixer"), require("postcss-nested")],
+      },
+    },
+  });
 
   // Return configuration object
   return {
