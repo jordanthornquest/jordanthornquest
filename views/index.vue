97e946fc2f66a2db08bc65108307382110002cd3
Jordan Thornquest <1444819+jordanthornquest@users.noreply.github.com>
2021-03-06 07:11:38 +0000

    it’s a vue template

---
+++ views/index.vue
@@ -0,0 +1,10 @@
+<template>
+  <main>
+    <h1>Reconstructing, be back soon.</h1>
+    <p>
+      For the time being, check out my
+      <a href="https://music.jordanthornquest.com">Bandcamp</a>. See you
+      shortly!
+    </p>
+  </main>
+</template>
