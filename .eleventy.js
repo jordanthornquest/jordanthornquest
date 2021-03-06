c850f3922e3b4bcb5d82b0c9522d5bbeeb4e3c51
Jordan Thornquest <1444819+jordanthornquest@users.noreply.github.com>
2021-03-06 08:06:43 +0000

    Remove config object

--- .eleventy.js
+++ .eleventy.js
@@ -1,17 +1,4 @@
-// Plugins
-const eleventyVue = require("@11ty/eleventy-plugin-vue");
-
 module.exports = function (eleventyConfig) {
-  // Add Eleventy Vue plugin, use defaults
-  eleventyConfig.addPlugin(eleventyVue, {
-    // Use postcss in the single file components
-    rollupPluginVueOptions: {
-      style: {
-        postcssPlugins: [require("autoprefixer"), require("postcss-nested")],
-      },
-    },
-  });
-
   // Return configuration object
   return {
     dir: {
