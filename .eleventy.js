e5e39b62ebd6d711f287337b3f0f40109e939f0e
Jordan Thornquest <1444819+jordanthornquest@users.noreply.github.com>
2021-03-06 07:06:44 +0000

    Add eleventy vue plugin

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,17 @@
-module.exports = {
-  dir: {
-    input: "views",
-    output: "dist",
-    includes: "_includes",
-    layouts: "_layouts",
-  },
+// Plugins
+const eleventyVue = require("@11ty/eleventy-plugin-vue");
+
+module.exports = function (eleventyConfig) {
+  // Add Eleventy Vue plugin, use defaults
+  eleventyConfig.addPlugin(eleventyVue);
+
+  // Return configuration object
+  return {
+    dir: {
+      input: "views",
+      output: "dist",
+      includes: "_includes",
+      layouts: "_layouts",
+    },
+  };
 };
