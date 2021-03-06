e9605887806285a56c545805d32f8a5adcc534d7
Jordan Thornquest <1444819+jordanthornquest@users.noreply.github.com>
2021-03-06 00:16:12 -0700

    no need for separate data file with vue

--- views/index.vue
+++ views/index.vue
@@ -8,3 +8,23 @@
     </p>
   </main>
 </template>
+
+<script>
+// Libraries
+const path = require("path");
+
+// Export
+export default {
+  data: {
+    description:
+      "Idaho musician Jordan Thornquest maps his mindstate with theatrical relish, producing a form of punk-informed power pop that's both candidly introspective and fearlessly extroverted.",
+    layout: "base.html",
+    socialImage:
+      "https://res.cloudinary.com/deedobzcr/image/upload/q_auto,f_auto,w_1200/v1559763975/hurr.jpg",
+    title: "Home",
+    eleventyComputed: {
+      pageFullUrl: (data) => path.join(data.constants.siteUrl, data.page.url),
+    },
+  },
+};
+</script>
