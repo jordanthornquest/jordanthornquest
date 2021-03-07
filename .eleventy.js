72f3eec85f72168b88834356ecc1091802e04c0c
Jordan Thornquest <1444819+jordanthornquest@users.noreply.github.com>
2021-03-07 01:49:52 +0000

    Update asset locations, change colors

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,6 @@
 module.exports = function (eleventyConfig) {
   // Copy static files to output
-  eleventyConfig.addPassthroughCopy("static/");
+  eleventyConfig.addPassthroughCopy({ "static/": "/" });
 
   // Return configuration object
   return {
