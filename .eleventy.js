0c9effc612739618af697ffa4d43aa1afe1c40e1
Jordan Thornquest <1444819+jordanthornquest@users.noreply.github.com>
2021-04-12 02:53:14 +0000

    Get that css working

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,9 @@ module.exports = function (eleventyConfig) {
   // Copy static files to output
   eleventyConfig.addPassthroughCopy({ "static/": "/" });
 
+  // Watch css and rebuild when changed
+  eleventyConfig.addWatchTarget("./styles/");
+
   // Return configuration object
   return {
     dir: {
