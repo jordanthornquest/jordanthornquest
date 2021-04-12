ca068e9232aaac9c3b9905837db0071436b69e4a
Jordan Thornquest <1444819+jordanthornquest@users.noreply.github.com>
2021-04-12 04:25:06 +0000

    Update code for cleaner tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,10 @@ module.exports = {
   purge: ["./eleventy/**/*.html"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    container: {
+      center: true,
+      padding: "1rem",
+    },
   },
   variants: {
     extend: {},
