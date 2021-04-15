f5a5882e7faf5d468201b963302143dc20d612fb
Jordan Thornquest <1444819+jordanthornquest@users.noreply.github.com>
2021-04-15 21:15:13 +0000

    fix paths for content

--- .eleventy.js
+++ .eleventy.js
@@ -1,20 +1,20 @@
 module.exports = function (eleventyConfig) {
   // Copy static files to output
-  eleventyConfig.addPassthroughCopy({ "static/": "/" });
+  eleventyConfig.addPassthroughCopy({ "./src/_static/": "./static/" });
 
   // Copy temporary CSS when watching
   eleventyConfig.addPassthroughCopy({
-    "src/_assets/css/compiled.css": "/css/styles.css",
+    "./src/_assets/css/compiled.css": "./css/styles.css",
   });
 
   // Return configuration object
   return {
     dir: {
-      data: "_data",
-      includes: "_includes",
-      input: "src",
-      layouts: "_layouts",
-      output: "dist",
+      data: "./_data",
+      includes: "./_includes",
+      input: "./src",
+      layouts: "./_layouts",
+      output: "./dist",
     },
   };
 };
