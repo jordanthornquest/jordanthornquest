c605cb7f25db78dccd4e71ff835d0cb9a706fabd
Jordan Thornquest <1444819+jordanthornquest@users.noreply.github.com>
2021-04-15 20:58:04 +0000

    copy compiled as styles in our output

--- .eleventy.js
+++ .eleventy.js
@@ -4,7 +4,7 @@ module.exports = function (eleventyConfig) {
 
   // Copy temporary CSS when watching
   eleventyConfig.addPassthroughCopy({
-    "src/_assets/css/styles.css": "/css/styles.css",
+    "src/_assets/css/compiled.css": "/css/styles.css",
   });
 
   // Return configuration object
