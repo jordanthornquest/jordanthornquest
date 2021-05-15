0bbfb12e9890902eb51554669edf0fe0a996f880
Jordan Thornquest <1444819+jordanthornquest@users.noreply.github.com>
2021-05-14 18:21:30 -0600

    move data into template instead of separate file

--- src/index.11ty.js
+++ src/index.11ty.js
@@ -1,18 +1,46 @@
 // Import lit-html-server to render templates
-const { html, renderToBuffer } = require("@popeindustries/lit-html-server");
+const { html, renderToString } = require("@popeindustries/lit-html-server");
 
-// Render the page with a function
-module.exports = async function (data) {
-  const pageContents = html`
-    <main class="container mt-5">
-      <h1>Reconstructing, be back soon.</h1>
-      <p class="mt-2">
-        For the time being, check out my
-        <a href="https://music.jordanthornquest.com">Bandcamp</a>. See you
-        shortly!
-      </p>
-    </main>
-  `;
+// Import path for building pageFullUrl
+const path = require("path");
 
-  return renderToBuffer(pageContents);
+// Contents for the page
+const pageContents = html`
+  <main class="container mt-5">
+    <h1>Reconstructing, be back soon.</h1>
+    <p class="mt-2">
+      For the time being, check out my
+      <a href="https://music.jordanthornquest.com">Bandcamp</a>. See you
+      shortly!
+    </p>
+  </main>
+`;
+
+// Data for the page
+const pageData = {
+  description:
+    "Jordan Thornquest maps his mindstate with theatrical relish & a form of punk-informed power pop that's both candidly introspective and fearlessly extroverted.",
+  layout: "base.11ty.js",
+  socialImage:
+    "https://res.cloudinary.com/deedobzcr/image/upload/q_auto,f_auto,w_1200/v1559763975/hurr.jpg",
+  title: "Home",
+  eleventyComputed: {
+    pageFullUrl: (data) => path.join(data.constants.siteUrl, data.page.url),
+  },
 };
+
+// Create page data and render class
+class Home {
+  // Assign data for page
+  async data() {
+    return pageData;
+  }
+
+  // Render page contents
+  async render() {
+    return renderToString(pageContents);
+  }
+}
+
+// Export class to Eleventy
+module.exports = Home;
