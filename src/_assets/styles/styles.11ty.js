1116f44ce0ed29a89744e68e6d2f2079a83d9e6a
Jordan Thornquest <1444819+jordanthornquest@users.noreply.github.com>
2021-05-15 21:22:04 -0600

    use async/await syntax and add compile function

--- src/_assets/styles/styles.11ty.js
+++ src/_assets/styles/styles.11ty.js
@@ -18,6 +18,7 @@ const tailwindcss = require("tailwindcss");
 
 // Export class with configuration for processing
 module.exports = class {
+  // Define data for use in render()
   async data() {
     // Create path to styles.css
     const entryPath = path.join(__dirname, `/${ENTRY_FILE_NAME}`);
@@ -29,16 +30,38 @@ module.exports = class {
       entryPath,
       // This is where the CSS output will go
       permalink: `/styles/styles.css`,
-      // Use fs to open our raw CSS for processing
-      // Use synchronous to make sure this data is retrieved first
-      rawCss: fs.readFileSync(entryPath),
     };
   }
 
+  // Define compile function
+  async compile(entryPath) {
+    try {
+      // Get raw CSS from file
+      const rawCss = fs.readFileSync(entryPath);
+
+      // Process raw CSS with PostCSS
+      const processedCss = await postcss([autoprefixer, tailwindcss]).process(
+        rawCss,
+        { from: entryPath }
+      );
+
+      // Get the processed CSS string
+      const result = processedCss.css;
+
+      // Return the string
+      return result;
+    } catch (error) {
+      throw error;
+    }
+  }
+
   // Process raw CSS with PostCSS and return the result as a string
-  async render({ entryPath, rawCss }) {
-    return await postcss([autoprefixer, tailwindcss])
-      .process(rawCss, { from: entryPath })
-      .then((result) => result.css);
+  async render({ entryPath }) {
+    try {
+      const css = await this.compile(entryPath);
+      return css;
+    } catch (error) {
+      console.error(error);
+    }
   }
 };
