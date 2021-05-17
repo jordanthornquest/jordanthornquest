2bcac3109b092f9ecd7141af7dcf3265899da82e
Jordan Thornquest <1444819+jordanthornquest@users.noreply.github.com>
2021-05-17 02:15:33 -0600

    use simpler date-based hashing

--- src/_data/assets.js
+++ src/_data/assets.js
@@ -1,36 +1,15 @@
-// Use fs to read asset files
-const fs = require("fs");
-
-// Use fast-glob for getting files for hashing
-const glob = require("fast-glob");
-
-// Use path to build the file path to entry file
-const path = require("path");
-
-// Use MD5 for hash generation
-const md5 = require("md5");
-
 // CSS variables
 const cssEntryFile = "styles.css";
-const cssEntryDir = path.join(__dirname, "../", "_assets/", "styles/");
 
-// Create hash from entry file
-const cssHash = function (dir) {
-  const cssFiles = glob.sync([`${dir}/**/*.css`]);
-  const cssContent = cssFiles
-    .map((cssFile) => fs.readFileSync(cssFile))
-    .join("");
-  return md5(cssContent).slice(0, 8);
-};
+// Generate hash string from current date
+const hash = Date.now();
+const hashString = hash.toString();
 
 // Export data for use
 module.exports = async function () {
-  // Generate hash
-  const generatedCssHash = cssHash(cssEntryDir);
-
   // Return data
   return {
     cssEntryFile,
-    generatedCssHash,
+    hashString,
   };
 };
