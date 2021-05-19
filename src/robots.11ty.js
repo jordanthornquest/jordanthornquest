315e953dea86b49609347fe8c0380c3b29ee438a
Jordan Thornquest <1444819+jordanthornquest@users.noreply.github.com>
2021-05-18 23:36:38 -0600

    add robots

---
+++ src/robots.11ty.js
@@ -0,0 +1,25 @@
+// use outdent to make sure there's no leading indentation
+const outdent = require("outdent");
+
+// Export robots
+module.exports = class {
+  // Output to robots.txt
+  // Don't include this file in collections
+  async data() {
+    return {
+      permalink: "/robots.txt",
+      layout: null,
+      eleventyExcludeFromCollections: true,
+    };
+  }
+
+  // Render robots.txt
+  async render({ head }) {
+    return outdent`
+      User-agent: *
+      Allow: /
+
+      Sitemap: http://www.jordanthornquest.com/sitemap.xml
+    `;
+  }
+};
