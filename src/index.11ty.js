234eacc8c14d0555d0fad0e66008a66cc3b4f3be
Jordan Thornquest <1444819+jordanthornquest@users.noreply.github.com>
2021-05-31 19:50:04 -0600

    add image to index with picture component

--- src/index.11ty.js
+++ src/index.11ty.js
@@ -1,35 +1,48 @@
 // Import lit-html-server to render templates
-const { html, renderToBuffer } = require("@popeindustries/lit-html-server");
+const { html } = require("@popeindustries/lit-html-server");
 
 // Create page data and render class
 module.exports = class {
   // Assign data for page
   async data() {
     return {
+      landingImage: {
+        default: {
+          alt: "A photo of me, leaning forward, staring at the camera in a leather jacket.",
+          height: 1000,
+          src: "/_static/images/who-is-he-square.jpeg",
+          width: 1000,
+        },
+        responsive: [
+          {
+            breakpoint: "md",
+            height: 816,
+            hiRes: true,
+            src: "/_static/images/who-is-he-16-9.jpeg",
+            width: 1462,
+          },
+        ],
+      },
       layout: "base.11ty.js",
       title: "Home",
     };
   }
 
-  async templateBuilder() {
+  // Render page contents
+  async render({ landingImage, tailwind }) {
+    // Get Tailwind breakpoints
+    const { breakpoints } = tailwind;
+    // Return the template
     return html`
       <main>
         <section>
-          <picture>
-            <img
-              alt="A photo of me, leaning forward, staring at the camera in a leather jacket."
-              src="/images/start/who-is-he-square.jpeg"
-            />
-          </picture>
+          ${await this.picture(
+            landingImage.default,
+            landingImage.responsive,
+            breakpoints
+          )}
         </section>
       </main>
     `;
-  }
-
-  // Render page contents
-  async render() {
-    const template = await this.templateBuilder();
-    const renderedTemplate = await renderToBuffer(template);
-    return renderedTemplate;
   }
 };
