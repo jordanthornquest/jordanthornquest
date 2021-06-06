a73756c67fa55cab90fad665d2cca1c67bfa483a
Jordan Thornquest <1444819+jordanthornquest@users.noreply.github.com>
2021-06-05 22:44:02 -0600

    update 404 to use shortcode

--- src/404.11ty.js
+++ src/404.11ty.js
@@ -7,20 +7,24 @@ module.exports = class {
   async data() {
     // Explicitly name permalink to avoid subfolder output
     return {
+      fearsome: {
+        alt: "A fearsome dog-like creature sits on a mattress, its eyes glowing fearfully.",
+        height: 750,
+        src: "/_static/images/run.jpeg",
+        sizes: [320, "sm", "md"],
+        width: 1000
+      },
       layout: "base.11ty.js",
       permalink: "/404.html",
       title: "Oh fuck",
     };
   }
 
-  async templateBuilder() {
+  // Render page contents
+  async render({ fearsome, tailwind }) {
     return html`
       <main class="container">
-        <img
-          alt="A fearsome dog-like creature sits on a mattress, its eyes glowing fearfully."
-          class="mt-10 max-w-md"
-          src="https://res.cloudinary.com/jordanthornquest/image/upload/c_scale,w_1000/v1621403580/Dog/run.webp"
-        />
+        ${this.image(tailwind.breakpoints, fearsome)}
         <h1 class="mt-5">Bad news dude</h1>
         <p class="mt-3">This page doesn't exist, help. Help? Oh god.</p>
         <p class="mt-2">
@@ -28,12 +32,5 @@ module.exports = class {
         </p>
       </main>
     `;
-  }
-
-  // Render page contents
-  async render() {
-    const template = await this.templateBuilder();
-    const renderedTemplate = await renderToBuffer(template);
-    return renderedTemplate;
   }
 };
