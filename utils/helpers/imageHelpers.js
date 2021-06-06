b79c56f146ec57f520695404170f1f00f4f4ebf6
Jordan Thornquest <1444819+jordanthornquest@users.noreply.github.com>
2021-06-05 21:04:40 -0600

    add image helpers to build responsive image attributes

---
+++ utils/helpers/imageHelpers.js
@@ -0,0 +1,41 @@
+// Build out srcset string based on responsive size keys
+const srcsetBuilder = async function (breakpoints, sizes, src) {
+  // Create a srcset array to store srcset values
+  const srcsetArray = [];
+
+  // Create a srcset string for each breakpoint value
+  // Add each result to srcset array
+  for await (let s of sizes) {
+    // Get breakpoint value from Tailwind breakpoints
+    // Size must be a string with the breakpoint key
+    let bpVal = breakpoints[s];
+
+    // Create srcset value from breakpoint value and src
+    let srcsetValue = `${src}?nf_resize=fit&w=${bpVal} ${bpVal}w`;
+
+    // Push value to srcset array
+    srcsetArray.push(srcsetValue);
+  }
+
+  // Convert all values in srcset array to a single srcset string
+  const srcsetString = srcsetArray.join(", ");
+
+  // Return srcset string
+  return srcsetString;
+};
+
+// Build out the media attribute for <source> tags
+const mediaBuilder = async function (bpVal) {
+  return `(min-width: ${bpVal}px)`;
+};
+
+// Build out the sizes attribute for <source> tags
+const sizesBuilder = async function (bpVal) {
+  return `(min-width: ${bpVal}px) ${bpVal}px, 100vw`;
+};
+
+module.exports = {
+  mediaBuilder,
+  sizesBuilder,
+  srcsetBuilder,
+};
