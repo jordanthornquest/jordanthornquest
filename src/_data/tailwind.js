4ba2e4d53de41f8844936d7718ffb080b9b29dcd
Jordan Thornquest <1444819+jordanthornquest@users.noreply.github.com>
2021-06-05 21:01:26 -0600

    use try blocks for async business

--- src/_data/tailwind.js
+++ src/_data/tailwind.js
@@ -2,41 +2,54 @@
 const resolveConfig = require("tailwindcss/resolveConfig");
 const tailwindConfig = require("../../tailwind.config.js");
 
-// Build Tailwind data from config
-const tailwindBuilder = async function (configFile) {
-  // use the Tailwind config to get elements
-  const tailwindData = resolveConfig(configFile);
+// Convert breakpoint values to numbers
+const breakpointBuilder = async function (breakpoints) {
+  try {
+    // Create new object for converted breakpoints
+    const newBreakpoints = {};
 
-  // Return fullConfig
-  return tailwindData;
+    // Convert the breakpoints to numbers without px values
+    for (let b in breakpoints) {
+      const value = breakpoints[b];
+      const newValue = value.replace("px", "");
+      const newNumber = Number(newValue);
+      newBreakpoints[b] = newNumber;
+    }
+
+    // Return new breakpoints
+    return newBreakpoints;
+  } catch (e) {
+    console.error(e);
+  }
 };
 
-// Build breakpoints values without 'px' appended
-const breakpointBuilder = async function (breakpoints) {
-  // Create new object for converted breakpoints
-  const newBreakpoints = {};
+// Build Tailwind data from config
+const tailwindBuilder = async function (configFile) {
+  try {
+    // use the Tailwind config to get elements
+    const tailwindData = resolveConfig(configFile);
 
-  // Convert the breakpoints to numbers without px values
-  for (let b in breakpoints) {
-    const value = breakpoints[b];
-    const newValue = value.replace("px", "");
-    const newNumber = Number(newValue);
-    newBreakpoints[b] = newNumber;
+    // Return fullConfig
+    return tailwindData;
+  } catch (e) {
+    console.error(e);
   }
-
-  // Return new breakpoints
-  return newBreakpoints;
 };
 
 // Export data for use
 module.exports = async function () {
-  // Get Tailwind data
-  const tailwindData = await tailwindBuilder(tailwindConfig);
+  try {
+    // Get Tailwind data
+    const tailwindData = await tailwindBuilder(tailwindConfig);
 
-  // Get breakpoints
-  const breakpoints = await breakpointBuilder(tailwindData.theme.screens);
-  // Return data
-  return {
-    breakpoints,
-  };
+    // Get breakpoints
+    const breakpoints = await breakpointBuilder(tailwindData.theme.screens);
+
+    // Return data
+    return {
+      breakpoints,
+    };
+  } catch (e) {
+    console.error(e);
+  }
 };
