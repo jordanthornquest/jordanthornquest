220a8141f6737e6af9eb222b34b5e4d972b3d90d
Jordan Thornquest <1444819+jordanthornquest@users.noreply.github.com>
2021-06-05 22:28:52 -0600

    fix social image

--- src/_data/head.js
+++ src/_data/head.js
@@ -2,5 +2,5 @@
 module.exports = {
   siteUrl: "https://www.jordanthornquest.com",
   socialImage:
-    "/_static/images/who-is-he-16-9.png",
+    "https://www.jordanthornquest.com/static/images/who-is-he-16-9.png",
 };
